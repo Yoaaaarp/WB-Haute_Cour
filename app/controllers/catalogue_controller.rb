a286632d02ee9539ae7954cabbd1a5cd10519220
David Villa <david.villa@heig-vd.ch>
2016-06-14 13:34:21 +0200

    finalisation vue concernant les commandes

--- app/controllers/catalogue_controller.rb
+++ app/controllers/catalogue_controller.rb
@@ -17,5 +17,4 @@ class CatalogueController < ApplicationController
       params.require(:number)
     end
 
-  
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,7 @@ class OrdersController < ApplicationController
   # GET /orders.json
   def index
     @orders = Order.all
+    @statuses = Status.all
   end
 
   # GET /orders/1
@@ -15,16 +16,19 @@ class OrdersController < ApplicationController
   # GET /orders/new
   def new
     @order = Order.new
+    @statuses = Status.all
   end
 
   # GET /orders/1/edit
   def edit
+    @statuses = Status.all
   end
 
   # POST /orders
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    @statuses = Status.all
 
     respond_to do |format|
       if @order.save
@@ -40,6 +44,8 @@ class OrdersController < ApplicationController
   # PATCH/PUT /orders/1
   # PATCH/PUT /orders/1.json
   def update
+    @statuses = Status.all
+
     respond_to do |format|
       if @order.update(order_params)
         format.html { redirect_to @order, notice: 'Order was successfully updated.' }
@@ -69,6 +75,7 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.fetch(:order, {})
+      #params.fetch(:order, {})
+      params.require(:order).permit(:user_id, :status_id)
     end
 end
